ca48c9759916fd533e1af60ebcc8145279315f44
evgeniidatsiuk <evgenii.datsiuk@gmail.com>
2019-05-18 19:02:25 +0300

    Add gem devise && create model user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_18_155226) do
+ActiveRecord::Schema.define(version: 2019_05_18_160139) do
 
   create_table "images", force: :cascade do |t|
     t.integer "user_id"
@@ -18,6 +18,18 @@ ActiveRecord::Schema.define(version: 2019_05_18_155226) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
